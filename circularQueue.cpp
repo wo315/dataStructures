0c830d6d5b44fc79c38e1f127a8ee720e744f7f2
wo315 <2106936@qq.com>
2020-04-26 01:10:26 +0800

    pushMethod

--- circularQueue.cpp
+++ circularQueue.cpp
@@ -1,17 +1,41 @@
 #include <iostream>
-#include <vector>
-#include <string>
+using std::cout;
+using std::cin;
+using std::endl;
 
-using namespace std;
+template <typename Type> class Queue {
+    private:
+        Type *data;
+        int head, tail, length, count;
+    public:
+        Queue(int input_size) {
+            length = input_size;
+            data = new Type[length];
+            head = 0;
+            tail = -1;
+            count = 0;
+        }
+        ~Queue() {
+            delete []data;
+        }
 
-int main()
-{
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-    
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+        bool push(Type element) {
+            if (tail + 1 >= length) {
+                return false;
+            }
+            tail++;
+            data[tail] = element;
+            return true;
+        }
+        void output() {
+            for (int i = head; i < tail; i++)
+            {
+                cout << data[i] << " ";
+            }
+            cout << data[tail] << endl;
+        }
+};
+
+int main() {
+    return 0;
 }
-
