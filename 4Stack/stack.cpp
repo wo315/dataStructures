b02ad0727f3d8267a02a05f853d237ae609673fd
wo315 <2106936@qq.com>
2020-04-26 15:07:25 +0800

    template stack constructor and destructor

--- 4Stack/stack.cpp
+++ 4Stack/stack.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <string>
+using std::cout;
+using std::cin;
+using std::string;
+
+template<typename Type> class Stack {
+    private:
+        Type *elements;
+        int max_size, top_index;
+
+    public:
+        Stack(int length_input) {
+            elements = new Type[length_input];
+            max_size = length_input;
+            top_index = -1;
+        }
+
+        ~Stack() {
+            delete []elements;
+        }
+
+};
+
+
 
 int main() {
-    std::cout << "hello" << std::endl;
+    int n;
+    cin >> n;
+    Stack<string> stack(n);
     return 0;
 }
